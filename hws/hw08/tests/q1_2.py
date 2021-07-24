57adb0b9dbaed515bef3f35ea364cf92029dd9df
Diraj Thajali <thajalidiraj@gmail.com>
2021-07-23 18:20:30 -0700

    finished hw08

---
+++ hws/hw08/tests/q1_2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_2',
+    'points': [0],
+    'suites': [   {   'cases': [   {'code': '>>> True\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
